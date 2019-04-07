9ed31acc38156fecdf99108f4e72c2f4d399aec2
Chi Chi Huynh <chichi.huynh@outlook.com>
2019-04-07 17:04:50 -0400

    made chatroom div clickable

--- project2/static/index.js
+++ project2/static/index.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     var username;
+    var chatroomCurrent;
     var chatroomList;
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    function createClickableChatroom(name) {
+        /*
+        Creates a chatroom that has a clickable event.
+        Once chatroom is clicked, messages are displayed and
+        user is able to send messages in that chatroom
+        */
+        const div = document.createElement('div');
+        div.innerHTML = `${name}`;
+        div.dataset.name = name;
+        div.className = 'chatroom-card';
+        div.onclick = () => {
+            console.log("hello world");
+        }
+        return div;
+    }
 
     // remember username when user visits page
     username = localStorage.getItem('username');
@@ -19,12 +37,9 @@ document.addEventListener('DOMContentLoaded', () => {
             div.innerHTML = 'No chatrooms'
             div.id = 'no-chatrooms-div'
             document.querySelector('#chatrooms').prepend(div);
-        }
-        else {
+        } else {
             for (var chatroom in chatroomList) {
-                const div = document.createElement('div')
-                div.innerHTML = `${chatroom}`;
-                div.dataset.name = chatroom;
+                const div = createClickableChatroom(chatroom)
                 document.querySelector('#chatrooms').prepend(div);
             }
         }
@@ -42,23 +57,23 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#create-chatroom-btn').onclick = () => {
             const newChatroom = document.querySelector('#new-chatroom').value;
             if (!(newChatroom in chatroomList)) {
-                socket.emit('create chatroom', {'chatroom': newChatroom});
+                socket.emit('create chatroom', {
+                    'chatroom': newChatroom
+                })
             }
         }
     })
 
-    // update list of chatrooms and show new chatroom
+    // display new chatroom
     socket.on('new chatroom', data => {
-        // delete no chatrooms message
         const noChatrooms = document.querySelector('#no-chatrooms-div');
         if (noChatrooms) {
             noChatrooms.remove();
         }
 
         chatroomList = data.chatrooms;
-        const div = document.createElement('div');
-        div.innerHTML = `${data.newChatroom}`;
-        div.dataset.name = data.newChatroom;
+
+        const div = createClickableChatroom(data.newChatroom)
         document.querySelector('#chatrooms').prepend(div);
     })
 })

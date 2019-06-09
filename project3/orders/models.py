4f0e0c24073aca53b5128f78cfbe69d2dba0d20a
Chi Chi Huynh <chichi.huynh@outlook.com>
2019-06-09 14:59:37 -0400

    added menu/order item models

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Menu item types
+----------------------------------------------------
+"""
+
 class PizzaType(models.Model):
     name = models.CharField(max_length=64, unique=True)
     sm_topping_0 = models.DecimalField(max_digits=6, decimal_places=2)
@@ -17,30 +23,6 @@ class PizzaType(models.Model):
     def __str__(self):
         return self.name
 
-class PizzaTopping(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-
-    def __str__(self):
-        return f"{self.name}"
-
-class Pizza(models.Model):
-    pizza_type = models.ForeignKey(PizzaType, on_delete=models.CASCADE)
-    toppings = models.ManyToManyField(PizzaTopping, blank=True)
-    size = models.CharField(max_length=2, choices=(("sm", "small"), ("lg", "large")), default="sm")
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
-
-    def calculate_price(self):
-        num_toppings = self.toppings.all().count()
-        if num_toppings > 3:
-            num_toppings = "special"
-        self.price = eval("self.pizza_type." +
-                          str(self.size) +
-                          "_topping_" +
-                          str(num_toppings))
-
-    def __str__(self):
-        return f"{self.pizza_type.name} with {self.toppings.all().count()} toppings"
-
 class SubType(models.Model):
     name = models.CharField(max_length=64, unique=True)
     price_sm = price_sm = models.DecimalField(max_digits=6, decimal_places=2, null=True)
@@ -53,14 +35,6 @@ class SubType(models.Model):
             return f"{self.name}: small - ${self.price_sm:.2f}, large - None"
         else:
             return f"{self.name}: small - ${self.price_sm:.2f}, large - ${self.price_lg:.2f}"
-
-class SubTopping(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    price_sm = models.DecimalField(max_digits=6, decimal_places=2)
-    price_lg = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.name}: small - ${self.price_sm}, large - ${self.price_lg}"
 
 class PastaType(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -83,3 +57,118 @@ class PlatterType(models.Model):
 
     def __str__(self):
         return f"{self.name}: small - ${self.price_sm:.2f}, large - ${self.price_lg:.2f}"
+
+"""
+Toppings
+----------------------------------------------------
+"""
+class PizzaTopping(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class SubTopping(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    price_sm = models.DecimalField(max_digits=6, decimal_places=2)
+    price_lg = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}: small - ${self.price_sm}, large - ${self.price_lg}"
+
+"""
+Order Items
+----------------------------------------------------
+"""
+class Pizza(models.Model):
+    type = models.ForeignKey(PizzaType, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    size = models.CharField(max_length=2, choices=(("sm", "small"), ("lg", "large")), default="sm")
+    toppings = models.ManyToManyField(PizzaTopping, blank=True)
+
+    def calculate_price(self):
+        num_toppings = self.toppings.all().count()
+        if num_toppings > 3:
+            num_toppings = "special"
+        self.price = eval("self.type." +
+                          str(self.size) +
+                          "_topping_" +
+                          str(num_toppings))
+        self.save()
+
+    def get_price(self):
+        if self.price == 0.00:
+            self.calculate_price()
+        return self.price
+
+    def __str__(self):
+        return f"{self.size} {self.pizza_type.name} with {self.toppings.all().count()} toppings"
+
+class Sub(models.Model):
+    type = models.ForeignKey(SubType, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    size = models.CharField(max_length=2, choices=(("sm", "small"), ("lg", "large")), default="sm")
+    toppings = models.ManyToManyField(SubTopping, blank=True)
+
+    def calculate_price(self):
+        self.price = eval("self.type.price_" + str(self.size))
+        for topping in self.toppings.all():
+            self.price += eval("topping.price_" + str(self.size))
+        self.save()
+
+    def get_price(self):
+        if self.price == 0.00:
+            self.calculate_price()
+        return self.price
+
+    def __str__(self):
+        return f"{self.size} {self.type.name} with {self.toppings.all().count()} add ons"
+
+class Pasta(models.Model):
+    type = models.ForeignKey(PastaType, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+
+    def calculate_price(self):
+        self.price = self.type.price
+        self.save()
+
+    def get_price(self):
+        if self.price == 0.00:
+            self.calculate_price()
+        return self.price
+
+    def __str__(self):
+        return f"{self.type.name}: ${self.price:.2f}"
+
+class Salad(models.Model):
+    type = models.ForeignKey(PastaType, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+
+    def calculate_price(self):
+        self.price = self.type.price
+        self.save()
+
+    def get_price(self):
+        if self.price == 0.00:
+            self.calculate_price()
+        return self.price
+
+    def __str__(self):
+        return f"{self.type.name}: ${self.price:.2f}"
+
+class Platter(models.Model):
+    type = models.ForeignKey(PastaType, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    size = models.CharField(max_length=2, choices=(("sm", "small"), ("lg", "large")), default="sm")
+
+    def calculate_price(self):
+        self.price = eval("self.type.price_" + str(self.size))
+        self.save()
+
+    def get_price(self):
+        if self.price == 0.00:
+            self.calculate_price()
+        return self.price
+
+    def __str__(self):
+        return f"{self.type.name}: ${self.price:.2f}"
--- project3/orders/views.py
+++ project3/orders/views.py
@@ -1,16 +1,62 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 # Create your views here.
+@login_required
 def index(request):
     context = {
+        "username": request.user.username,
         "pizza_types": PizzaType.objects.all(),
         "pizza_toppings": PizzaTopping.objects.all(),
         "sub_types": SubType.objects.all(),
         "sub_toppings": SubTopping.objects.all(),
         "pastas": PastaType.objects.all(),
         "salads": SaladType.objects.all(),
-        "platters": PlatterType.objects.all()
+        "platters": PlatterType.objects.all(),
+        "sub": Sub.objects.first()
     }
     return render(request, "orders/index.html", context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect('index')
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+    form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "authentication/register.html", context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print(f"logged in as {username}")
+                return redirect('index')
+            else:
+                print(f"invalid username and/or password")
+        else:
+            print(f"invalid username and/or password")
+    form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "authentication/login.html", context)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
